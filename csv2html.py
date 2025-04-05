0a9b174305626d960cd1b573ec8bddccc1a59dba
Chris Barts <chbarts@gmail.com>
2025-04-05 04:49:26 -0600

    Initial commit

---
+++ csv2html.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import csv
+import html
+import time
+import argparse
+
+def csv2html(inf, outf, title):
+    outf.write("<html><head><meta charset=\"utf-8\"><title>{}</title></head><body>\n".format(html.escape(title)))
+    outf.write("<ol>\n")
+    reader = csv.DictReader(inf)
+    for row in reader:
+        if len(row['url']) == 0:
+            continue
+        title = row['url']
+        if len(row['title']) > 0:
+            title = row['title']
+        if len(row['last_visit_date']) > 0:
+            lvd = int(row['last_visit_date']) / 1000000
+            ltime = time.localtime(lvd)
+            stime = html.escape(time.strftime("%A, %B %d, %Y %T %z", ltime))
+            outf.write("<li>{} &mdash; <a href=\"{}\">{}</a></li>\n".format(stime, row['url'], html.escape(title)))
+        else:
+            outf.write("<li><a href=\"{}\">{}</a></li>\n".format(row['url'], html.escape(title)))
+    outf.write("</ol>\n")
+    outf.write("</body></html>\n")
+
+parser = argparse.ArgumentParser(description='Convert A Firefox moz_places Table CSV Dump to HTML')
+
+parser.add_argument('-i', '--input', metavar='INFILE', type=str, nargs=1, default='', help='Specify INFILE as RSS input file, defaults to stdin')
+parser.add_argument('-o', '--output', metavar='OUTFILE', type=str, nargs=1, default='', help='Specify OUTFILE as HTML output file, defaults to stdout')
+parser.add_argument('-t', '--title', metavar='TITLE', type=str, nargs=1, default='Firefox Places Dump', help='The title of the HTML file')
+
+args = parser.parse_args()
+
+if (len(args.input) > 0) and (len(args.output) > 0):
+    with open(args.input[0]) as inf:
+        with open(args.output[0], 'w') as outf:
+            csv2html(inf, outf, args.title)
+    sys.exit(0)
+elif len(args.input) > 0:
+    with open(args.input[0]) as inf:
+        csv2html(inf, sys.stdout, args.title)
+    sys.exit(0)
+elif len(args.output) > 0:
+    with open(args.output[0], 'w') as outf:
+        csv2html(sys.stdin, outf, args.title)
+    sys.exit(0)
+else:
+    csv2html(sys.stdin, sys.stdout, args.title)
+    sys.exit(0)
